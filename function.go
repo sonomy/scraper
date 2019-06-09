81b8033b9fa07092711bc05abcb87b061d5111f0
ms2sato <ms2.sato@gmail.com>
2019-06-09 13:01:00 +0900

    slimup

--- function.go
+++ function.go
@@ -14,7 +14,12 @@ func Handle(w http.ResponseWriter, r *http.Request) {
     projID := os.Getenv("DATASTORE_PROJECT_ID")
 	if projID == "" {
 		log.Fatal(`You need to set the environment variable "DATASTORE_PROJECT_ID"`)
+    }
+    host := os.Getenv("DATASTORE_EMULATOR_HOST")
+	if host != "" {
+		log.Println(`DATASTORE_EMULATOR_HOST alive`)
 	}
+
 	// [START datastore_build_service]
 	ctx := context.Background()
 	client, err := datastore.NewClient(ctx, projID)
