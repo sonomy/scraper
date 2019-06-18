4c7f30343abc761f5003f7cb5509bace43bc4647
ms2sato <ms2.sato@gmail.com>
2019-06-18 12:05:20 +0900

    datastore on another container

--- main/main.go
+++ main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "log"
     "fmt"
     "github.com/sonomy/scraper"
     "google.golang.org/appengine"
@@ -11,16 +10,8 @@ import (
 // @see https://gist.github.com/hoitomt/c0663af8c9443f2a8294
 
 func main() {
-    httpPort := 8082
+    httpPort := 8080
     fmt.Printf("> listening on %v\n", httpPort)
     http.HandleFunc("/scraper", scraper.Handle)
     appengine.Main()
-    // http.ListenAndServe(fmt.Sprintf(":%d", httpPort), logRequest(http.DefaultServeMux))
-}
-
-func logRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-		handler.ServeHTTP(w, r)
-	})
 }
